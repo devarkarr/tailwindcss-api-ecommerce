f431d32de241023c557fa7683caaa88d1d3b62e8
arkarlin2003 <arkarlin486@gmail.com>
2026-05-30 11:49:07 +0630

    feat(app): setup project

---
+++ services/products.js
@@ -0,0 +1,32 @@
+/** @format */
+async function getProdcuts() {
+  const url = "https://fakestoreapi.com/products";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getProdcut(id) {
+  const url = `https://fakestoreapi.com/products/${id}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export { getProdcuts, getProdcut };
